3ba045c91bcfe09b97fa1260c7422c7a19107746
explosivegreen <shoko6662@gmail.com>
2025-04-02 12:30:27 +0300

    refactor Permission component styles and consolidate utility styles into a single file

--- components/Permission.tsx
+++ components/Permission.tsx
@@ -1,6 +1,5 @@
 import { PermissionResponse } from "expo-camera";
-import { Button, Text, View } from "react-native";
-import styles from "../utils/style";
+import { Button, Dimensions, StyleSheet, Text, View } from "react-native";
 
 export default function Permission (props:{
     requestPermissions: () => Promise<PermissionResponse>;
@@ -14,3 +13,56 @@ export default function Permission (props:{
       </View>
     );
 }
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    alignSelf: 'center',
+    width: windowWidth,
+    height: windowHeight - 70,
+  },
+  image: {
+    margin: 5,
+    width: windowWidth,
+    height: windowHeight / 2.7,
+    alignSelf: 'center',
+    objectFit: 'contain',
+    backgroundColor: 'black',
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    justifyContent: 'space-around',
+    marginTop: 64,
+    marginBottom: 64,
+    marginLeft: 0,
+    marginRight: 0,
+  },
+  button: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  pointMarker: {
+    position: 'absolute',
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: 'red'
+  },
+  tableHeader: {
+    backgroundColor: '#DCDCDC',
+  },
+});
